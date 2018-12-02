20f3ecfebddf78eb816439fc9eb285bf50277d0a
David Crean <dcrean@kyriba.com>
2018-12-02 19:03:52 +0300

    fix day 1

--- 2018/01/index.mjs
+++ 2018/01/index.mjs
@@ -1,8 +1,27 @@
 import { inputData } from './input.mjs';
 
+const args = process.argv.slice(2);
 let frequency = 0;
-inputData.forEach(val => {
-  frequency += val;
-});
-console.log(frequency);
+let seen = {};
+if ( args[0] ) {
+  frequency = inputData.reduce((acc, val) => acc + val);
+  console.log(frequency);
+} else {
+  let seen = {
+    0: true,
+  };
+  let foundDupe = false;
+  while (!foundDupe) {
+    for (let i = 0; i < inputData.length; i++) {
+      frequency += inputData[i];
+      if (seen[frequency]) {
+        foundDupe = true; 
+        break;
+      } else {
+        seen[frequency] = true;
+      }      
+    }
+  }
+  console.log(frequency);
+}
 
